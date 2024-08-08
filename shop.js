e762cd2ef74d6b04f51be4217e5d395ed2514a81
Dugidem Egwuatu <Dugidemeg@gmail.com>
2024-08-08 02:20:40 +0100

    close modal functionality

--- shop.js
+++ shop.js
@@ -322,6 +322,11 @@ confirmOrder.addEventListener('click', () => {
     //? Populate the modal content
     const modalContent = document.querySelector('.modal');
     const html = `
+        <div class="close-modal">
+            <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="close-modal-icon">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z" fill-rule="evenodd"/>
+            </svg>
+        </div>
         <div class="modal-header">
             <img src="assets/images/icon-order-confirmed.svg" class="check-mark" alt="check-mark">
             <h2 class="modal-title">Order Confirmed</h2>
@@ -368,6 +373,13 @@ confirmOrder.addEventListener('click', () => {
         </button>
     `;
     modalContent.innerHTML += html;
+
+    const closeModal = document.querySelector('.close-modal-icon');
+    closeModal.addEventListener('click', () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        modalContent.innerHTML = '';
+    })
     
 });
 
